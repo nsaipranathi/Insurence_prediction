05b6aec9bcc66863533a0622a8f98b972585fb96
N. Sai Pranathi <149822584+nsaipranathi@users.noreply.github.com>
2026-03-11 12:23:30 +0530

    Update prediction.py

--- insurence_prediction/src/prediction.py
+++ insurence_prediction/src/prediction.py
@@ -1,23 +1,28 @@
 import pickle
+import os
 
 class Insurance_Prediction:
 
     def __init__(self):
-        with open("../artifacts/scaler.pkl", "rb") as f:
+
+        # find project root folder
+        base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+        scaler_path = os.path.join(base_dir, "artifacts", "scaler.pkl")
+        model_path = os.path.join(base_dir, "artifacts", "model.pkl")
+
+        with open(scaler_path, "rb") as f:
             self.scaler = pickle.load(f)
 
-        with open("../artifacts/model.pkl", "rb") as f:
+        with open(model_path, "rb") as f:
             self.model = pickle.load(f)
 
     def prediction(self, Age, Annual_Income_LPA, Policy_Term_Years, Sum_Assured_Lakhs):
 
-        # create input list
         data = [[Age, Annual_Income_LPA, Policy_Term_Years, Sum_Assured_Lakhs]]
 
-        # scale input
         scaled_data = self.scaler.transform(data)
 
-        # model prediction
         result = self.model.predict(scaled_data)
 
         return result[0]
